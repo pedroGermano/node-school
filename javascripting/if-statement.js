aebd71956b549d58bf995dadf4b184cd013757bb
Pedro Germano <pedrogermano232@gmail.com>
2024-03-24 17:53:35 -0300

    FEAT: array and numbers

---
+++ javascripting/if-statement.js
@@ -0,0 +1,7 @@
+const fruits = "orange";
+
+if (fruits.length > 5) {
+  console.log("The fruit name has more than five characters.");
+} else {
+  console.log("The fruit name has five characters or less.");
+}
